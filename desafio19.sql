18db92db9f37305dff8ff3329a0e9c307dcc4e4b
Bete.mrt <bete.mrt@gmail.com>
2023-01-20 20:45:36 -0300

    Requisito 19 rev

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT count(id) AS orders_count FROM northwind.orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
+SELECT count(id) AS orders_count FROM orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
